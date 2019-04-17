70d7f8b1166e2f202ff2842b57da0a9ac2bdecc4
RareScrap <rarescrap@users.noreply.github.com>
2019-04-18 00:32:49 +0800

    build(core): Установил более поддящую версию и имя файла мода

--- src/main/java/ru/rarescrap/simpleweightsystem/ConfigurableWeight.java
+++ src/main/java/ru/rarescrap/simpleweightsystem/ConfigurableWeight.java
@@ -40,7 +40,7 @@ import static ru.rarescrap.simpleweightsystem.Utils.drawCenteredStringWithoutSha
 public class ConfigurableWeight
 {
     public static final String MODID = "configurableweight";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "0.0.1";
 
     public static final SimpleNetworkWrapper NETWORK = NetworkRegistry.INSTANCE.newSimpleChannel(MODID.toLowerCase());
 
